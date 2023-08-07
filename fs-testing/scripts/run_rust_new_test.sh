711391ccd6ccda2cad71322693752e6fbc5a4b5f
Christian Oder <me@myself5.de>
2023-08-07 16:19:23 +0200

    fs-testing: Add additional rust tests

--- fs-testing/scripts/run_rust_new_test.sh
+++ fs-testing/scripts/run_rust_new_test.sh
@@ -4,20 +4,13 @@ set -eu -o pipefail
 script=$0
 scriptdir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 base=$scriptdir/../..
-results=results/rust-test
+results=results/rust-new-test
 
 rm -rf "$results"
-mkdir -p "$results"/{vinter_rust_legacy,vinter_rust}
+mkdir -p "$results"/vinter_rust
 
 test=test_hello-world
-vms=("vm_nova" "vm_pmfs")
-
-# Analysis with vinter_rust_legacy
-for vm in "${vms[@]}"; do
-  echo "Running vinter_rust_legacy with test $test on $vm..."
-  "$base/target/release/vinter_trace2img_legacy" analyze --output-dir "$results/vinter_rust_legacy" \
-    "$scriptdir/$vm.yaml" "$scriptdir/$test.yaml"
-done
+vms=("vm_nova")
 
 # Analysis with vinter_rust using the new crash image generator
 for vm in "${vms[@]}"; do
